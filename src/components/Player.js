4e2447f6d15d5568d519d9af0fc2c60bdbfb8e02
Mikhaiel_Offical <89293342+mikhaiel0@users.noreply.github.com>
2024-08-31 15:23:02 +0530

    `music streaming`

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,1 +1,13 @@
+import React from 'react';
+
+function Player({ currentSong }) {
+    return (
+        <div className="player">
+            <h2>Now Playing</h2>
+            <p>{currentSong ? currentSong.title : "Select a song to play"}</p>
+        </div>
+    );
+}
+
+export default Player;
 
