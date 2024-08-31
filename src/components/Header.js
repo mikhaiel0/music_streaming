085958e218fd6340d6a4a4fa80feedb2a6f75428
Mikhaiel_Offical <89293342+mikhaiel0@users.noreply.github.com>
2024-08-31 15:21:27 +0530

    `music streaming`

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,13 @@
+import React from 'react';
+
+function Header({ user }) {
+    return (
+        <header className="app-header">
+            <h1>Hi There, {user}</h1>
+            <input type="text" placeholder="Search for songs, albums, or artists" />
+        </header>
+    );
+}
+
+export default Header;
 
