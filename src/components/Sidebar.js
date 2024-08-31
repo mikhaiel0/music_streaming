e6d73dbb17026d8a85876cf1f518ebf02fad1470
Mikhaiel_Offical <89293342+mikhaiel0@users.noreply.github.com>
2024-08-31 15:22:26 +0530

    `music streaming`

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,1 +1,16 @@
+import React from 'react';
+
+function Sidebar() {
+    return (
+        <aside className="sidebar">
+            <ul>
+                <li>Home</li>
+                <li>Playlists</li>
+                <li>Search</li>
+            </ul>
+        </aside>
+    );
+}
+
+export default Sidebar;
 
